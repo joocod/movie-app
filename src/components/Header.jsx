71bc11c1ab0aaf1caab8a123c187207566238779
joocod <20634hdj@gmail.com>
2023-12-05 14:28:35 +0900

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <HeaderContainer>
+                <h1 className='logo'>movie</h1>
+            </HeaderContainer>
+        </div>
+    )
+}
+
+export default Header
